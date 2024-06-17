38f9f95d23be487b003cf18202f5a0a43d2d8a1d
prxska <ictskills4@gmail.com>
2024-06-17 00:57:12 -0400

    integracion

---
+++ js/registrar.js
@@ -0,0 +1,56 @@
+// fade de pagina 
+$(document).ready(function() {
+
+    $('#loginModal').on('show.bs.modal', function() {
+        $('body').addClass('no-fade');
+    }).on('hidden.bs.modal', function() {
+        $('body').removeClass('no-fade');
+    });
+
+    if (!$('body').hasClass('no-fade')) {
+        $('body').css('display', 'none');
+        $('body').fadeIn(2000);
+    }
+});
+
+
+
+//MODO OSCURO 
+
+const toggleButton = document.getElementById('themeToggle');
+const themeIcon = document.getElementById('themeIcon');
+const body = document.body;
+
+toggleButton.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+    if (body.classList.contains('dark-mode')) {
+        themeIcon.classList.remove('fa-moon');
+        themeIcon.classList.add('fa-sun');
+    } else {
+        themeIcon.classList.remove('fa-sun');
+        themeIcon.classList.add('fa-moon');
+    }
+});
+
+// Guardar el tema seleccionado en localStorage
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme) {
+    body.classList.add(savedTheme);
+    if (savedTheme === 'dark-mode') {
+        themeIcon.classList.remove('fa-moon');
+        themeIcon.classList.add('fa-sun');
+    }
+}
+
+// Guardar la preferencia del usuario
+toggleButton.addEventListener('click', () => {
+    if (body.classList.contains('dark-mode')) {
+        localStorage.setItem('theme', 'dark-mode');
+    } else {
+        localStorage.setItem('theme', 'light-mode');
+    }
+});
+
+
+
+
