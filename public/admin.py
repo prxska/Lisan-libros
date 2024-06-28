978a8791761c7902732de5a56b853d2565fb434c
prxska <ictskills4@gmail.com>
2024-06-27 22:23:30 -0400

    se agrego la visualizacion de los atributos de forma legible en /admin

--- public/admin.py
+++ public/admin.py
@@ -7,5 +7,8 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'email', 'password')
     search_fields = ('first_name', 'last_name', 'email')
 
+
+
+
 # Register your models here.
 admin.site.register(User, UserAdmin)
